4f49870b4cb8badcd2ec127296cb3c95e2a96169
Travis Whitehead <whitehet@osuosl.org>
2018-06-29 13:25:44 -0700

    Initial Commit of osl-imap

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+osl-imap CHANGELOG
+==================
+This file is used to list changes made in each version of the
+osl-imap cookbook.
+
+0.1.0
+-----
+- Initial release of osl-imap
+
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             'osl-imap'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apachev2'
+issues_url       'https://github.com/osuosl-cookbooks/osl-imap/issues'
+source_url       'https://github.com/osuosl-cookbooks/osl-imap'
+description      'Installs/Configures osl-imap'
+long_description 'Installs/Configures osl-imap'
+version          '0.1.0'
+
+supports         'centos', '~> 7.0'
+
+depends          'dovecot'

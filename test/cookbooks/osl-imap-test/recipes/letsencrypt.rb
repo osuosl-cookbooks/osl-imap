eebb20659a4b3c6834d40079faf4cf142e71ea34
Lance Albertson <lance@osuosl.org>
2019-09-13 16:20:45 -0700

    Add support for LetsEncrypt
    Well sort of... This adds an attribute which makes it easier to integrate this
    with LE in other cookbooks. I decided not to actually implement LE directly in
    the cookbook since the actual deployment of LE can vary widely depending on the
    need. In the case for phpBB, this would not have worked since we're using this
    cert for a webmail portal as well which might not be the case in other places.
    
    However, I did test this with a test recipe using LE to ensure it actually works
    properly!

---
+++ test/cookbooks/osl-imap-test/recipes/letsencrypt.rb
@@ -0,0 +1,35 @@
+include_recipe 'osl-acme::server'
+include_recipe 'osl-apache'
+include_recipe 'apache2::mod_ssl'
+include_recipe 'osl-acme'
+
+cert_path = '/etc/pki/tls/imap.osuosl.org.crt'
+chain_path = '/etc/pki/tls/imap.osuosl.org-chain.crt'
+key_path = '/etc/pki/tls/imap.osuosl.org.key'
+
+apache_app 'imap.osuosl.org' do
+  directory '/var/www/imap.osuosl.org'
+  ssl_enable true
+  cert_file cert_path
+  cert_key key_path
+  cert_chain chain_path
+end
+
+acme_selfsigned 'imap.osuosl.org' do
+  crt cert_path
+  chain chain_path
+  key key_path
+  notifies :restart, 'service[apache2]', :immediately
+end
+
+acme_certificate 'imap.osuosl.org' do
+  crt cert_path
+  chain chain_path
+  key key_path
+  wwwroot '/var/www/imap.osuosl.org/'
+  notifies :restart, 'service[apache2]'
+end
+
+node.default['dovecot']['conf']['ssl'] = 'required'
+node.default['dovecot']['conf']['ssl_cert'] = '</etc/pki/tls/imap.osuosl.org.crt'
+node.default['dovecot']['conf']['ssl_key']  = '</etc/pki/tls/imap.osuosl.org.key'
